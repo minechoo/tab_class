80e1bf6e0d710c9315b770fa5a38d443f4df40b7
minechoo <choominhae@gmail.com>
2023-06-20 12:33:02 +0900

    전개연산자

--- js/es6.js
+++ js/es6.js
@@ -1,8 +1,16 @@
 class Tab {
 	constructor(selector, option) {
+		//defaults, results값은 굳이 인스턴스 객체에 전달할 필요가 없으므로
+		//지역변수로 설정
+		//this 객체에 특정 정보값을 복사해서 전달하는 구조이므로
+		//가급적이면 this로 복사해야할 정보값을 줄이는 걸 권장
+		//defaults 와 result 값을 인스턴스 객체에서 직접적으로 필요한 값이 아니므로 지역변수로 설정
+		//해당 생성자를 통해서 복사가 될때마다 지역변수로 설정한 값을 static하게 유지가 됨
+		const defaults = { btns: 'ul li', boxs: 'article div' };
+		const result = { ...defaults, ...option };
 		this.tab = document.querySelector(selector);
-		this.btns = this.tab.querySelectorAll(option.btns);
-		this.boxs = this.tab.querySelectorAll(option.boxs);
+		this.btns = this.tab.querySelectorAll(result.btns);
+		this.boxs = this.tab.querySelectorAll(result.boxs);
 		this.bindingEvent();
 	}
 
@@ -15,14 +23,7 @@ class Tab {
 	}
 
 	activation(arr, idx) {
-		arr.forEach((el) => {
-			el.classList.remove('on');
-		});
+		arr.forEach((el) => el.classList.remove('on'));
 		arr[idx].classList.add('on');
 	}
 }
-
-new Tab('#tab', {
-	btns: 'ul li',
-	boxs: 'article div',
-});
